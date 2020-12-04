0cba06d29c7afe751e5d6422499cca895660638e
yijian.song <yijian.song@sigmob.com>
2020-12-04 17:38:40 +0800

    修复Array解析错误

--- src/tool.js
+++ src/tool.js
@@ -1,28 +1,23 @@
-import {isObj, isArray /*, isRegExp,isString,isNumber,isBoolean*/} from './isfn' 
-
-
+import {isObj, isArray, isString} from './isfn' 
+  
 export const parseMuster= {
   String,
   Number,
   Boolean,
   Object:(a)=>{
-    let obj = JSON.parse(a)
-    if(isObj(obj))return obj;
+    if(isObj(a))return a;
+    if(isString(a)){
+      let obj = JSON.parse(a)
+      if(isObj(obj))return obj
+    };
     throw Error('is not Object')
   },
   Array:(a)=>{
-    let array = JSON.parse(a)
-    if(isArray(array)) return array
+    if(isArray(a))return a;
+    if(isString(a)){
+      let array = JSON.parse(a)
+      if(isArray(array)) return array
+    }
     throw Error('is not Array')
   }
 }
-
-// export const typeMuster = {
-//   String:isString,
-//   Number:isNumber,
-//   Boolean:isBoolean,
-//   Object:isObj,
-//   Array:isArray
-//   // RegExp,
-// }
-
